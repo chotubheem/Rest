363c5d0298eb805eb9133aa421b36fc5e9a14deb
Medha Chakraborty <107214299+IMDC0@users.noreply.github.com>
2023-10-04 21:37:16 +0530

    First commit

---
+++ RestAssuredAutomation/src/test/java/testdemo/GetandPostExample.java
@@ -0,0 +1,26 @@
+package testdemo;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.baseURI;
+
+import org.testng.annotations.Test;
+
+
+
+@Test
+public class GetandPostExample {
+	
+	public void get () {
+		
+		baseURI = "https://reqres.in/api";
+		
+		given().
+		get("/users?page=2").
+		then().
+		statusCode(200).
+		log().all();
+		
+		
+	}
+
+}
